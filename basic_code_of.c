85bdaf25740f32d518bee545b5a4a15ef0cfbc43
mahdikohee <mahdikohee@gmail.com>
2025-07-02 19:47:04 +0600

    Update basic_code_of.c

--- basic_code_of.c
+++ basic_code_of.c
@@ -1,1 +1,8 @@
-
+//A simple code example of void and pointer ;
+#include<stdio.h>
+int main(void){
+    int number = 120 ;
+    void *ptr = &number ;
+    printf("Your number is %d\n" , *(int *)ptr) ;
+    return 0 ;
+}
