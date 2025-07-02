09d08ee9e115741debb727d1f61ec9fa9d661e91
mahdikohee <mahdikohee@gmail.com>
2025-07-02 20:02:18 +0600

    Update basic_code_of.c

--- basic_code_of.c
+++ basic_code_of.c
@@ -6,3 +6,18 @@ int main(void){
     printf("Your number is %d\n" , *(int *)ptr) ;
     return 0 ;
 }
+
+//another c code example of void , ptr , struct and typedef 
+#include<stdio.h>
+typedef struct{
+    char name[20] ;
+    int age ;
+}Person ;
+
+int main(void){
+    Person p1 = {"kohee" , 21} ;
+    void *ptr = &p1 ;
+    printf("The name is %s\n" , ((Person*)ptr)->name) ;
+    printf("The age is :%d\n" , ((Person*)ptr)->age) ;
+
+}
